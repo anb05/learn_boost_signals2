34f46feb3a154b0d4c50a43c4efe81d323a38e33
alexandr05 <alexandr05@list.ru>
2020-05-09 22:32:57 +0300

    created sceleton

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/signals2/signal.hpp>
+#include <cstdint>
+
+int32_t main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
+{
+    
+    return 0;
+}
+
+
