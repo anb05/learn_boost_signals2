9060ef4a1047aa22b75cb3a9061deabb8a70eafe
alexandr05 <alexandr05@list.ru>
2020-05-09 23:14:36 +0300

    step 2. Multiple Slot

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <boost/signals2/signal.hpp>
 #include <cstdint>
 
-auto hw = [](){std::cout << "Hellow world. Hi boost signals2!!!" << std::endl;};
+auto hw = [](){std::cout << "Hellow";};
 
 struct hw_func {
     void operator()() {
-        std::cout << "Hellow world from func. obj. !!!" << std::endl;
+        std::cout << ", world!!!" << std::endl;
     };
 };
 
@@ -14,15 +14,13 @@ int32_t main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
 {
     // step 1. create signal.
     boost::signals2::signal<void()> sig;
-    boost::signals2::signal<void()> sig1;
 
     // step 2. connect hw slot.
     sig.connect(hw);
-    sig1.connect(hw_func());
+    sig.connect(hw_func());
 
     // step3. call all of the slot;
     sig();
-    sig1();
 
     return 0;
 }
